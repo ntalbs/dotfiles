71c2e9af8cca45b67cce00a9b1a11ad26d6b6c97
ntalbs <ntalbsen@gmail.com>
2015-01-11 17:41:43 +0900

    postgresql 관련 설정 추가.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,6 +3,10 @@ set -x JAVA8_HOME (/usr/libexec/java_home -v1.8)
 set -x JAVA7_HOME (/usr/libexec/java_home -v1.7)
 set -x JAVA_HOME $JAVA8_HOME
 
+set -x PGDATA /usr/local/pgsql/data
+
 alias java8 "set -x JAVA_HOME $JAVA8_HOME"
 alias java7 "set -x JAVA_HOME $JAVA7_HOME"
 alias bu "brew update; brew upgrade; brew cleanup; brew doctor"
+alias pg_start "pg_ctl start -l logfile"
+alias pg_stop "pg_ctl stop"
