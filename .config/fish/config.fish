744fb7df4feda4ad6e1a340b53cec606094cef6b
ntalbs <ntalbsen@gmail.com>
2015-03-18 16:36:35 +0900

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,8 @@ set -x JAVA_HOME $JAVA8_HOME
 
 set -x PGDATA /usr/local/pgsql/data
 
+set -x EDITOR vim
+
 alias java8 "set -x JAVA_HOME $JAVA8_HOME"
 alias java7 "set -x JAVA_HOME $JAVA7_HOME"
 alias bu "brew update; brew upgrade; brew cleanup; brew doctor"
