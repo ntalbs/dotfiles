d7a8478344eba240a350f992c4cc71e731d5792d
ntalbs <ntalbsen@gmail.com>
2015-04-13 07:36:33 +0900

    ANDROID_HOME 환경변수 추가.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@ set -x PATH ~/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin
 set -x JAVA8_HOME (/usr/libexec/java_home -v1.8)
 set -x JAVA7_HOME (/usr/libexec/java_home -v1.7)
 set -x JAVA_HOME $JAVA8_HOME
+set -x ANDROID_HOME /usr/local/opt/android-sdk
 
 set -x PGDATA /usr/local/pgsql/data
 
