d372a8cbc0b18cb25b99512befba76e0bd78f07c
ntalbs <ntalbsen@gmail.com>
2016-01-22 14:08:25 +0000

    설정 변경.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,14 @@
-set -x PATH ~/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin
+set -x SHELL /usr/local/bin/fish
+set -x PATH /apollo/env/SDETools/bin ~/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin
 set -x JAVA8_HOME (/usr/libexec/java_home -v1.8)
-set -x JAVA7_HOME (/usr/libexec/java_home -v1.7)
 set -x JAVA_HOME $JAVA8_HOME
-set -x ANDROID_HOME /usr/local/opt/android-sdk
 
-set -x PGDATA /usr/local/pgsql/data
-
+#set -x ANDROID_HOME /usr/local/opt/android-sdk 
+#set -x PGDATA /usr/local/pgsql/data 
 set -x EDITOR vim
 
 alias java8 "set -x JAVA_HOME $JAVA8_HOME"
-alias java7 "set -x JAVA_HOME $JAVA7_HOME"
 alias bu "brew update; brew upgrade --all; brew cleanup; brew doctor"
-alias pg_start "pg_ctl start -l /usr/local/pgsql/data/logfile"
-alias pg_stop "pg_ctl stop"
+
+#alias pg_start "pg_ctl start -l /usr/local/pgsql/data/logfile"
+#alias pg_stop "pg_ctl stop"
