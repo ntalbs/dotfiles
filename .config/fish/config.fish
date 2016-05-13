6b22f109c2ae4719c7ed0be43ee95415a361d453
ntalbs <ntalbsen@gmail.com>
2016-05-13 15:27:23 +0100

    postgresql 관련 설정 uncomment.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,11 +4,12 @@ set -x JAVA8_HOME (/usr/libexec/java_home -v1.8)
 set -x JAVA_HOME $JAVA8_HOME
 
 #set -x ANDROID_HOME /usr/local/opt/android-sdk 
-#set -x PGDATA /usr/local/pgsql/data 
+set -x PGDATA /usr/local/pgsql/data
 set -x EDITOR vim
 
 alias java8 "set -x JAVA_HOME $JAVA8_HOME"
 alias bu "brew update; brew upgrade --all; brew cleanup; brew doctor"
 
-#alias pg_start "pg_ctl start -l /usr/local/pgsql/data/logfile"
-#alias pg_stop "pg_ctl stop"
+alias pg_start "pg_ctl start -l /usr/local/pgsql/data/logfile"
+alias pg_stop "pg_ctl stop"
+
