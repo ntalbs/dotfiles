90b499132d0a7af64fc1d12e6b3f0415ae884ef0
ntalbs <ntalbsen@gmail.com>
2016-05-21 17:59:58 +0100

    불필요 설정 및 경로 삭제.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,8 @@
 set -x SHELL /usr/local/bin/fish
-set -x PATH /apollo/env/SDETools/bin ~/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin
+set -x PATH /apollo/env/SDETools/bin /usr/local/bin /usr/bin /bin /usr/sbin /sbin
 set -x JAVA8_HOME (/usr/libexec/java_home -v1.8)
 set -x JAVA_HOME $JAVA8_HOME
 
-#set -x ANDROID_HOME /usr/local/opt/android-sdk 
 set -x PGDATA /usr/local/pgsql/data
 set -x EDITOR vim
 
@@ -12,4 +11,3 @@ alias bu "brew update; brew upgrade --all; brew cleanup; brew doctor"
 
 alias pg_start "pg_ctl start -l /usr/local/pgsql/data/logfile"
 alias pg_stop "pg_ctl stop"
-
