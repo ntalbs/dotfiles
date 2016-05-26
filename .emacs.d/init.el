7e27ba598ecea00284bba4a86415109ee6342533
ntalbs <ntalbsen@gmail.com>
2016-05-26 19:19:05 +0100

    hl-line 백그라운스 색상 변경

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -76,7 +76,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(hl-line ((t (:background "##aef"))))
+ '(hl-line ((t (:background "#ffa"))))
  '(italic ((t (:slant normal))))
  '(js2-function-call ((t (:inherit default :foreground "dark blue"))))
  '(markdown-bold-face ((t (:inherit font-lock-variable-name-face :foreground "Red" :weight semi-bold))))
