1d649fafcd4006a681543a9e9d7320622d8eccf8
ntalbs <ntalbsen@gmail.com>
2016-07-11 09:52:26 +0100

    mc에 대해 use-package 사용하도록 수정

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -98,7 +98,7 @@
 (exec-path-from-shell-initialize)
 
 ;; flycheck
-(require 'package)
+;(require 'package)
 (use-package flycheck
   :ensure t
   :init (global-flycheck-mode))
@@ -159,11 +159,11 @@
 (add-to-list 'mmm-mode-ext-classes-alist '(markdown-mode nil markdown-java))
 
 ;; multiple cursors
-(require 'multiple-cursors)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-c C->") 'mc/mark-all-like-this)
+(use-package multiple-cursors
+  :bind (("C-S-c C-S-c" . mc/edit-lines)
+         ("C->" . mc/mark-next-like-this)
+         ("C-<" . mc/mark-previous-like-this)
+         ("C-c C->" . mc/mark-all-like-this)))
 
 ;; Set up unicode
 ;; (set-terminal-coding-system 'utf-8)
