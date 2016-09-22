39d818b1576c4ce30a552b6c444be50b37ff4b6d
ntalbs <ntalbsen@gmail.com>
2016-09-22 07:30:51 +0100

    org-mode에서 yasnippet 확장 안 되는 문제 수정

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -163,7 +163,16 @@
 (use-package yasnippet
   :init
   (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
-  (yas-global-mode t))
+  (yas-global-mode t)
+  :config
+  (defun yas/org-very-safe-expand ()
+    (let ((yas-fallback-behavior 'return-nil)) (yas-expand)))
+  (add-hook 'org-mode-hook
+            (lambda ()
+              (make-variable-buffer-local 'yas/trigger-key)
+              (setq yas/trigger-key [tab])
+              (add-to-list 'org-tab-first-hook 'yas/org-very-safe-expand)
+              (define-key yas-keymap [tab] 'yas/next-field))))
 
 (use-package auto-complete
   :init
