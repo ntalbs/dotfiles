7a8f2a5c36cb7b50c17e529e1cd2459628422ca9
ntalbs <ntalbsen@gmail.com>
2017-10-05 12:30:03 +0100

    Java9 설정 추가

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,12 +2,14 @@ set -x SHELL /usr/local/bin/fish
 set -x BRAZIL_CLI_BIN /apollo/env/SDETools/bin
 set -x PATH $BRAZIL_CLI_BIN /usr/local/crux /usr/local/bin /usr/local/sbin /usr/bin /bin /usr/sbin /sbin /usr/local/share/git-core/contrib/diff-highlight /workplace/AVClientServicesDevScripts/bin
 set -x JAVA8_HOME (/usr/libexec/java_home -v1.8)
+set -x JAVA9_HOME (/usr/libexec/java_home -v9)
 set -x JAVA_HOME $JAVA8_HOME
 
 set -x PGDATA /usr/local/pgsql/data
 set -x EDITOR vim
 
 alias java8 "set -x JAVA_HOME $JAVA8_HOME"
+alias java9 "set -x JAVA_HOME $JAVA9_HOME"
 alias bu "brew update; brew upgrade --all; brew cleanup; brew doctor"
 
 alias pg_start "pg_ctl start -l /usr/local/pgsql/data/logfile"
