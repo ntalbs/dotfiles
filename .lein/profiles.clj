3d1615203ebeee59d38b90c5b66870585c2a8917
ntalbs <ntalbsen@gmail.com>
2018-02-22 18:19:59 +0000

    Update cider-nrepl version

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,3 @@
 {:repl {:plugins [[refactor-nrepl "2.4.0-SNAPSHOT"]
-                  [cider/cider-nrepl "0.16.0-SNAPSHOT"]]
+                  [cider/cider-nrepl "0.17.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
