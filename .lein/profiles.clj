cb205d901d96a6407ecb4a643d89a7ea4661cf72
ntalbs <ntalbsen@gmail.com>
2019-02-21 00:03:01 +0000

    Update projects.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,2 @@
-{:repl {:plugins [[refactor-nrepl "2.4.0-SNAPSHOT"]
-                  [cider/cider-nrepl "0.18.0"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
+{:repl {:plugins [[lein-pprint "1.1.1"]
+                  [cider/cider-nrepl "0.21.1"]]}}
