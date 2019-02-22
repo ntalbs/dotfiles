2ff8e1491c606bc439e1622f419909cdd7742530
ntalbs <ntalbsen@gmail.com>
2019-02-22 11:54:52 +0000

    Remove markdown-command
    This preview doesn't work well. Better to use hugo directly.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -340,8 +340,7 @@
   :commands (markdown-mode gfm-mode)
   :mode (("README\\.md\\'" . gfm-mode)
          ("\\.md\\'" . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode))
-  :init (setq markdown-command "multimarkdown"))
+         ("\\.markdown\\'" . markdown-mode)))
 
 (use-package restclient
   :config
