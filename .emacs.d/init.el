ec4bf7a372da45cd1f5d86221e76d818427e71c3
ntalbs <ntalbsen@gmail.com>
2019-03-06 17:31:24 +0000

    update init.el: change hl-line background

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -91,7 +91,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(hl-line ((t (:background "#fff"))))
+ '(hl-line ((t (:background "#ccc"))))
  '(js2-function-call ((t (:inherit default :foreground "dark blue"))))
  '(markdown-bold-face ((t (:inherit font-lock-variable-name-face :foreground "Red" :weight semi-bold))))
  '(markdown-header-face ((t (:inherit font-lock-function-name-face :weight semi-bold))))
