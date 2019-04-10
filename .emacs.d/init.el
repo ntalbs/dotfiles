d34b8a37522031ece83a7e20b4e87ff6df96c257
ntalbs <ntalbsen@gmail.com>
2019-04-10 13:22:07 +0100

    Change company popup colors

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -96,6 +96,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-scrollbar-bg ((t (:background "dark gray"))))
+ '(company-scrollbar-fg ((t (:background "dark blue"))))
+ '(company-template-field ((t (:background "LightBlue1" :foreground "black"))))
+ '(company-tooltip ((t (:background "light gray" :foreground "black"))))
  '(hl-line ((t (:background "#ccc"))))
  '(js2-function-call ((t (:inherit default :foreground "dark blue"))))
  '(lsp-ui-sideline-code-action ((t (:foreground "SpringGreen4"))))
